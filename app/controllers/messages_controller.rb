a62088ae8945ccfe7b0a88cafe6a067a67d3a148
satoshi-kenmotsu <skem209@gmail.com>
2020-09-05 20:23:46 +0900

    form_haml

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,11 +7,11 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = @group.messages.new(message_params)
+    @message = @user.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      redirect_to user_messages_path(@user), notice: 'メッセージが送信されました'
     else
-      @messages = @group.messages.includes(:user)
+      @messages = @user.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
