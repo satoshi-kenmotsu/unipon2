70c3e1153e1f3e5188fbcbdc45df45754fda8883
satoshi-kenmotsu <skem209@gmail.com>
2020-09-16 20:12:29 +0900

    resource_error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :users, only: [:edit, :update]
-  resources :messages, only: [:index, :create]
+  resources :users, only: [:edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
