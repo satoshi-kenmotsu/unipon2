b356122c288a37bf1e2b0370ca6ee4a67c4d9b2b
satoshi-kenmotsu <skem209@gmail.com>
2020-09-24 17:21:25 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
   has_many :messages
   has_many :likes
   has_many :liked_messages, through: :likes, source: :message
+  def already_liked?(message)
+    self.likes.exists?(message_id: message.id)
+  end
 end
