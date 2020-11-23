73b3cf3f259fe3c445ecd2fba0711f405225a92d
satoshi-kenmotsu <skem209@gmail.com>
2020-11-23 17:52:17 +0900

    c

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :users, only: [:edit, :update] do
+  resources :users, only: [:edit, :update, :show] do
     resources :messages, only: [:index, :create, :destroy, :edit, :update] do
       resources :likes, only: [:create, :destroy]
     end
